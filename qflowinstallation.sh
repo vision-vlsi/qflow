99438e9788e4835a468848a8b68188090d20b701
vision-vlsi <89384474+vision-vlsi@users.noreply.github.com>
2021-09-11 10:56:44 +0530

    Update qflowinstallation.sh

--- qflowinstallation.sh
+++ qflowinstallation.sh
@@ -1,13 +1,38 @@
+echo
+echo
+echo
+echo "**************************************"
+echo  "WelCome to Opensource Installation"
+echo         "Installation script"
+echo
+echo                 "By"
+echo
+echo       "Vision-VLSI Pvt. Ltd."
+echo    "We offer Training in VLSI Domain"
+echo
+echo       "Contact at +919949426362"
+echo "**************************************"
+echo
+echo
+echo
+echo "**************************************"
+echo        "Installation Begins"
+echo "**************************************"
+echo
+echo
+echo
 cd
 cd /var/lib/dpkg/
 sudo rm lock lock-frontend
 cd
 sudo apt-get install build-essential clang bison flex libreadline-dev gawk tcl-dev libffi-dev git graphviz xdot pkg-config python3 libboost-system-dev libboost-python-dev libboost-filesystem-dev zlib1g-dev --assume-yes
+echo "Cloning yosys and will start installation of yosys"
 git clone https://github.com/YosysHQ/yosys.git
 cd yosys/
 sudo make
 sudo make install
 cd
+echo "Cloning graywolf and will start installation of graywolf"
 git clone https://github.com/rubund/graywolf.git
 cd graywolf/
 mkdir build
@@ -19,6 +44,7 @@ cmake ..
 sudo make  
 sudo make install  
 cd
+echo "Cloning qrouter and will start installation of qrouter"
 git clone https://github.com/RTimothyEdwards/qrouter.git
 cd qrouter/
 sudo apt-get install tcl-dev tk-dev --assume-yes 
@@ -33,18 +59,21 @@ sudo apt-get install libx11-dev --assume-yes
 sudo apt-get install libcairo2-dev --assume-yes
 sudo apt-get install mesa-common-dev libglu1-mesa-dev --assume-yes
 sudo apt-get install libncurses-dev --assume-yes
+echo "Cloning magic and will start installation of magic"
 git clone https://github.com/RTimothyEdwards/magic.git
 cd magic/
 ./configure 
 sudo make
 sudo make install
 cd
+echo "Cloning netgen and will start installation of netgen"
 git clone https://github.com/RTimothyEdwards/netgen.git
 cd netgen/
 ./configure 
 sudo make
 sudo make install
 cd
+echo "Cloning OpenSTA and will start installation of OpenSTA"
 git clone https://github.com/The-OpenROAD-Project/OpenSTA.git
 cd OpenSTA/
 mkdir build
@@ -54,6 +83,7 @@ cmake ..
 sudo make
 sudo make install
 cd
+echo "Cloning qflow and will start installation of qflow"
 git clone https://github.com/RTimothyEdwards/qflow.git
 cd qflow/
 ./configure 
