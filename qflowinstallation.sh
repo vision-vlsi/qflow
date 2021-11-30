c5eae789e2708f92c804c394998a2e9e08fc6b2a
vision-vlsi <89384474+vision-vlsi@users.noreply.github.com>
2021-11-30 08:12:55 +0530

    Update qflowinstallation.sh

--- qflowinstallation.sh
+++ qflowinstallation.sh
@@ -22,29 +22,36 @@ echo
 echo
 echo
 cd
-cd /var/lib/dpkg/
-sudo rm lock lock-frontend
 cd
+echo "*********************************************************************************************************"
 echo
 echo
-echo "****************************************  Installing yosys dependancies   *********************************************"
+echo "****************************************  Installing yosys dependancies   ************************"
 echo
 echo
+echo "*********************************************************************************************************"
 sudo apt-get install build-essential clang bison flex libreadline-dev gawk tcl-dev libffi-dev git graphviz xdot pkg-config python3 libboost-system-dev libboost-python-dev libboost-filesystem-dev zlib1g-dev --assume-yes
 echo
-echo "************************************** Cloning yosys and will start installation of yosys ********************************"
+echo
+echo "*********************************************************************************************************"
 echo
 echo
+echo "*************************** Cloning yosys and will start installation of yosys ********************"
+echo
+echo
+echo "*********************************************************************************************************"
 git clone https://github.com/YosysHQ/yosys.git
 cd yosys/
 sudo make
 sudo make install
 cd
+echo "*********************************************************************************************************"
 echo
 echo
-echo "************************************** Cloning graywolf and will start installation of graywolf **************************************"
+echo "********************** Cloning graywolf and will start installation of graywolf ************************"
 echo
 echo
+echo "*********************************************************************************************************"
 git clone https://github.com/rubund/graywolf.git
 cd graywolf/
 mkdir build
@@ -56,11 +63,13 @@ cmake ..
 sudo make  
 sudo make install  
 cd
+echo "*********************************************************************************************************"
 echo
 echo
-echo "************************************** Cloning qrouter and will start installation of qrouter**************************************"
+echo "********************** Cloning qrouter and will start installation of qrouter **************************"
 echo
 echo
+echo "*********************************************************************************************************"
 git clone https://github.com/RTimothyEdwards/qrouter.git
 cd qrouter/
 sudo apt-get install tcl-dev tk-dev --assume-yes 
@@ -68,11 +77,13 @@ sudo apt-get install tcl-dev tk-dev --assume-yes
 sudo make
 sudo make install
 cd
+echo "*********************************************************************************************************"
 echo
 echo
-echo "*********************************  Installing Magic dependancies   ******************************************"
+echo "************************  Installing Magic dependancies   *******************************"
 echo
 echo
+echo "*********************************************************************************************************"
 sudo apt-get install m4 --assume-yes
 sudo apt-get install tcsh --assume-yes
 sudo apt-get install csh --assume-yes
@@ -82,7 +93,15 @@ sudo apt-get install mesa-common-dev libglu1-mesa-dev --assume-yes
 sudo apt-get install libncurses-dev --assume-yes
 echo
 echo
-echo "**************************************  Cloning magic and will start installation of magic  **************************************"
+echo "*********************************************************************************************************"
+echo "*********************************************************************************************************"
+echo
+echo
+echo "*********************  Cloning magic and will start installation of magic  ************************"
+echo
+echo
+echo "*********************************************************************************************************"
+echo "*********************************************************************************************************"
 echo
 echo
 git clone https://github.com/RTimothyEdwards/magic.git
@@ -93,7 +112,15 @@ sudo make install
 cd
 echo
 echo
-echo "**************************************  Cloning netgen and will start installation of netgen  **************************************"
+echo "*********************************************************************************************************"
+echo "*********************************************************************************************************"
+echo
+echo
+echo "*******************  Cloning netgen and will start installation of netgen  ****************"
+echo
+echo
+echo "*********************************************************************************************************"
+echo "*********************************************************************************************************"
 git clone https://github.com/RTimothyEdwards/netgen.git
 echo
 echo
@@ -102,7 +129,15 @@ cd netgen/
 sudo make
 sudo make install
 cd
-echo "**************************************  Cloning OpenSTA and will start installation of OpenSTA  **************************************"
+echo "*********************************************************************************************************"
+echo "*********************************************************************************************************"
+echo
+echo
+echo "*******************************  Cloning OpenSTA and will start installation of OpenSTA  ****************"
+echo
+echo
+echo "*********************************************************************************************************"
+echo "*********************************************************************************************************"
 git clone https://github.com/The-OpenROAD-Project/OpenSTA.git
 cd OpenSTA/
 mkdir build
@@ -114,7 +149,15 @@ sudo make install
 cd
 echo
 echo
-echo "**************************************  Cloning qflow and will start installation of qflow  **************************************"
+echo "*********************************************************************************************************"
+echo "*********************************************************************************************************"
+echo
+echo
+echo "****************************  Cloning qflow and will start installation of qflow  ************************"
+echo
+echo
+echo "*********************************************************************************************************"
+echo "*********************************************************************************************************"
 git clone https://github.com/RTimothyEdwards/qflow.git
 echo
 echo
@@ -125,12 +168,29 @@ sudo make install
 sudo apt-get install -y python3-tk --assume-yes
 echo
 echo
-echo "************************************************ Installing klayout ******************************************************"
+echo "*********************************************************************************************************"
+echo "*********************************************************************************************************"
+echo
+echo
+echo "************************************************ Installing klayout **************************************"
+echo
+echo
+echo "*********************************************************************************************************"
+echo "*********************************************************************************************************"
 echo
 echo
 sudo apt install klayout --assume-yes
+echo
+echo
+echo
+echo "*********************************************************************************************************"
+echo
 echo
 echo "**************************************************************************************"
 echo "                             Installation Completed                                   "
 echo "**************************************************************************************"
 echo
+echo
+echo
+echo
+echo "*********************************************************************************************************"
