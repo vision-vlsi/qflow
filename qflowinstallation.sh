e3cc79ce0d5830dd024182cab8bb3499ec8b51e9
vision-vlsi <89384474+vision-vlsi@users.noreply.github.com>
2022-01-02 19:41:41 +0530

    Update qflowinstallation.sh

--- qflowinstallation.sh
+++ qflowinstallation.sh
@@ -32,6 +32,7 @@ echo
 echo "*********************************************************************************************************"
 sudo apt-get install build-essential clang bison flex libreadline-dev gawk tcl-dev libffi-dev git graphviz xdot pkg-config python3 libboost-system-dev libboost-python-dev libboost-filesystem-dev zlib1g-dev --assume-yes
 echo
+sudo apt-get update
 echo
 echo "*********************************************************************************************************"
 echo
