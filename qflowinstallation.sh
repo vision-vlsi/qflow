5b91f2eac22991647fb03ee524d3c9b9d3a5a2a8
vision-vlsi <89384474+vision-vlsi@users.noreply.github.com>
2022-01-07 14:57:29 +0530

    Update qflowinstallation.sh

--- qflowinstallation.sh
+++ qflowinstallation.sh
@@ -30,6 +30,7 @@ echo "****************************************  Installing yosys dependancies   
 echo
 echo
 echo "*********************************************************************************************************"
+sudo apt-get update
 sudo apt-get install build-essential clang bison flex libreadline-dev gawk tcl-dev libffi-dev git graphviz xdot pkg-config python3 libboost-system-dev libboost-python-dev libboost-filesystem-dev zlib1g-dev --assume-yes
 echo
 sudo apt-get update
